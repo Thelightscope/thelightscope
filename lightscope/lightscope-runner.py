a2fcd1fb70c79bf69a6c86579d1324b8b66f585a
erickapitanski <e@alumni.usc.edu>
2025-07-16 20:05:00 -0700

    scary build changes to rpm build

--- lightscope/lightscope-runner.py
+++ lightscope/lightscope-runner.py
@@ -47,7 +47,7 @@ else:
     LOGS_DIR = LIGHTSCOPE_HOME / "logs"
     BIN_DIR = LIGHTSCOPE_HOME / "bin"
 
-runner_version = "1.0.0"
+runner_version = "1.0.1"
 
 print(f"runner_version: {runner_version}")
 
